c99a2c2418d6e950951ac83c484fac0c547a265d
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-03-07 17:22:17 +0100

    Adds ENV variables

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 var config = {
-  SERVER_PORT: 3000
+  SERVER_PORT: process.env.SERVER_PORT || 3000,
+  PAYLOAD_MAX_SIZE: process.env.PAYLOAD_MAX_SIZE || 1048576
 }
 
 module.exports = config
